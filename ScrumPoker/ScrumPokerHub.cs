b6817792566c3afc096d49112efda68e5abb612e
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2025-03-17 18:49:50 -0300

    localstorage e gerenciando reconexão

--- ScrumPoker/ScrumPokerHub.cs
+++ ScrumPoker/ScrumPokerHub.cs
@@ -2,14 +2,28 @@
 
 public class ScrumPokerHub : Hub
 {
-    private static readonly Dictionary<string, string?> Votes = new();
+    private static readonly Dictionary<string, (string ConnectionId, string? Vote)> Votes = new();
 
     public async Task JoinGame(string userName)
     {
-        if (!Votes.ContainsKey(userName))
+        var existingUser = Votes.FirstOrDefault(x => x.Key == userName);
+        if (!string.IsNullOrEmpty(existingUser.Key))
         {
-            Votes[userName] = null;
+            Votes.Remove(existingUser.Key);
+        }
+
+        Votes[userName] = (Context.ConnectionId, null);
+        await Clients.All.SendAsync("UpdateUsers", Votes.Keys);
+        //await Clients.All.SendAsync("UpdateVotes", Votes);
+    }
+
+    public async Task LeaveGame(string userName)
+    {
+        if (Votes.ContainsKey(userName))
+        {
+            Votes.Remove(userName);
             await Clients.All.SendAsync("UpdateUsers", Votes.Keys);
+            await Clients.All.SendAsync("UpdateVotes", Votes);
         }
     }
 
@@ -17,22 +31,35 @@ public class ScrumPokerHub : Hub
     {
         if (Votes.ContainsKey(userName))
         {
-            Votes[userName] = vote;
-            await Clients.All.SendAsync("UpdateVotes", Votes);
+            Votes[userName] = (Votes[userName].ConnectionId, vote);
+            await Clients.All.SendAsync("UpdateVotes", Votes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Vote));
         }
     }
 
     public async Task RevealVotes()
     {
-        await Clients.All.SendAsync("RevealVotes", Votes);
+        await Clients.All.SendAsync("RevealVotes", Votes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Vote));
     }
 
     public async Task ResetVotes()
     {
-        foreach (var key in Votes.Keys)
+        foreach (var key in Votes.Keys.ToList())
         {
-            Votes[key] = null;
+            Votes[key] = (Votes[key].ConnectionId, null);
         }
-        await Clients.All.SendAsync("UpdateVotes", Votes);
+        await Clients.All.SendAsync("UpdateVotes", Votes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Vote));
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var user = Votes.FirstOrDefault(x => x.Value.ConnectionId == Context.ConnectionId);
+        if (!string.IsNullOrEmpty(user.Key))
+        {
+            Votes.Remove(user.Key);
+            await Clients.All.SendAsync("UpdateUsers", Votes.Keys);
+            await Clients.All.SendAsync("UpdateVotes", Votes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Vote));
+        }
+
+        await base.OnDisconnectedAsync(exception);
     }
 }
